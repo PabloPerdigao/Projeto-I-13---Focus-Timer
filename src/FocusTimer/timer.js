23e1462d3f3202d4318a05b8f96aab61e679bcf7
Pablo Perdigão <pabloagps@gmail.com>
2024-06-30 11:51:05 -0300

    criamos o arquivo timer.js, contendo a função updateDisplay, e as modificações nos arquivos elements.js e index.js. Agora, o display do contador será atualizado corretamente com os valores dos minutos e segundos.

---
+++ src/FocusTimer/timer.js
@@ -0,0 +1,14 @@
+import state from './state.js'
+import * as el from './elements.js'
+
+export function updateDisplay(minutes, seconds) {
+  /* nullish coalesing operator*/ 
+  minutes = minutes ?? state.minutes
+  seconds = seconds ?? state.seconds
+
+  /* preenchendo com 0 */
+  el.minutes.textContent = String(minutes).padStart(2, "0")
+  el.seconds.textContent = String(seconds).padStart(2, "0")
+
+
+}

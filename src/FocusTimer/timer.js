86f3dce83be8c9587344df011c79b417103d8871
Pablo Perdigão <pabloagps@gmail.com>
2024-06-30 12:42:53 -0300

    No arquivo actions.js, adicionamos a chamada da função updateDisplay no método reset, para atualizar o display quando o timer for resetado.
    No arquivo timer.js, implementamos a função countdown que decrementa os valores de minutos e segundos a cada segundo. Se os segundos chegam a zero, decrementamos os minutos e ajustamos os segundos para 59. Se os minutos chegam a zero, chamamos a função reset para encerrar o countdown.

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,16 +1,42 @@
 import state from './state.js'
 import * as el from './elements.js'
+import { reset } from './actions.js'
 
 export function countDown() {
   if(!state.isRunning) {
     return
   }
 
-  console.log('iniciou')
+  let minutes = Number(el.minutes.textContent)
+  let seconds = Number(el.seconds.textContent)
 
-  // recursão - função callback que necessita de um parâmentro para ser determinada a parar em determinado momento
-   setTimeout(() => countDown(), 1000)
-  
+
+  //decrementar 1 segundo
+    seconds--
+
+  // verificar se segundo < 0 adicionar 59
+    if(seconds < 0) {
+      seconds = 59
+
+      //decrementar 1 segundo
+        minutes --
+    }
+
+  // verificar se minutos < 0 resetar o seu valor e depois reiniciar 
+    if(minutes < 0) {
+      reset()
+      return
+    }
+
+
+    updateDisplay(minutes, seconds)
+    
+    setTimeout(() => countDown(), 1000)
+
+    /* 
+      recursão - função que é chamada de volta e que necessita de um parâmentro a ser determinada para parar  a mesma em determinado momento
+      setTimeout - função callback ( função determinada em outro momento, podendo ser posteriormente chamada )
+    */
 }
 
 export function updateDisplay(minutes, seconds) {
